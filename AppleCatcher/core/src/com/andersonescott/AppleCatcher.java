83cd0077379b380e8c381b2749ab2556c312abb8
cescott <chuckescott@comcast.net>
2016-09-29 09:11:51 -0700

    I gotta
    gotta

--- AppleCatcher/core/src/com/andersonescott/AppleCatcher.java
+++ AppleCatcher/core/src/com/andersonescott/AppleCatcher.java
@@ -13,7 +13,7 @@ public class AppleCatcher extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("red_panda_02.jpg");
 	}
 
 	@Override
