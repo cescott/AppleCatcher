eff4f18ef765301123c8ef4a891f69459716a539
JayTheDonkey <jayander3.14159@gmail.com>
2016-10-06 09:41:54 -0700

    Started work on gameworld

--- AppleCatcher/core/src/com/andersonescott/gameworld/GameWorld.java
+++ AppleCatcher/core/src/com/andersonescott/gameworld/GameWorld.java
@@ -1,9 +1,20 @@
 package com.andersonescott.gameworld;
 
 
+import com.andersonescott.Apple;
+import com.andersonescott.Player;
+
 public class GameWorld {
 
+    protected Apple apple;
+    protected Player player;
+
+    public GameWorld(){
+        player = new Player(new double[] {300, 0}, 0);
+        apple = new Apple(new double[] {(Math.random()*800), 600});
+    }
     public void update(float delta){
+        player.update(delta);
 
     }
 }
