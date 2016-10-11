82a4b9860691b1db06d1c8fd7b4f1ff59958f920
cescott <chuckescott@comcast.net>
2016-10-11 09:39:18 -0700

    Hella apple updates
    hella

--- AppleCatcher/core/src/com/andersonescott/gameworld/GameWorld.java
+++ AppleCatcher/core/src/com/andersonescott/gameworld/GameWorld.java
@@ -10,19 +10,26 @@ public class GameWorld {
 
     protected ArrayList<Apple> apples= new ArrayList<Apple>();
     protected Player player;
+    protected int score;
+    protected int counter;
 
     public GameWorld(){
-        player = new Player(new double[] {300, 0}, 0);
-        apples.add(new Apple(new double[] {(Math.random()*740), 600}));
+        player = new Player(new double[] {350, 0}, 0);
+        score = 0;
     }
 
     public void update(float delta){
         //update player
         player.update(delta);
         //update all the apples
+        if (appleCount()) {
+            apples.add(new Apple(new double[]{(Math.random() * 700) + 10, 520}));
+        }
         for (int i=0; i<apples.size();i++){
             apples.get(i).update(delta);
         }
+        //update collisions
+        collisionUpdate();
     }
 
     public Player getPlayer(){
@@ -31,6 +38,27 @@ public class GameWorld {
 
     public ArrayList<Apple> getApples(){
         return apples;
+    }
+
+    public boolean appleCount() {
+        counter++;
+        if ((double)counter/30 >= (20/(score + 5)) + 0.25) {
+            counter = 0;
+            return true;
+        }
+        return false;
+    }
+
+    public void collisionUpdate() {
+        for (int i = 0; i < apples.size(); i++) {
+            if ((apples.get(i).x() <= 0 && apples.get(i).getVelocity()[0] < 0)
+                    || (apples.get(i).x() >= 720 && apples.get(i).getVelocity()[0] > 0)) {
+                apples.get(i).setVelocity(new double[]{apples.get(i).getVelocity()[0] * -1, apples.get(i).getVelocity()[1]});
+            }
+            if (apples.get(i).y() <= 2) {
+                apples.remove(i);
+            }
+        }
     }
 
 }
--- AppleCatcher/core/src/com/andersonescott/objects/Apple.java
+++ AppleCatcher/core/src/com/andersonescott/objects/Apple.java
@@ -9,7 +9,8 @@ public class Apple extends Entity {
     public Apple(double[] tempCoords){
         super(tempCoords);
         image = new Texture("applecatcher_apple.png");
-        acceleration = new double[]{0, -400};
+        acceleration = new double[]{0, -1200};
+        velocity = new double[]{Math.random()*200 - 100, 200};
     }
 
 }
